f1c4ae45dbbc4a529f4f3ea7555d13e71451af73
Abdelrahman Seyam <arseyam@hotmail.com>
2020-08-06 16:55:13 +0300

    updated message

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace GitHub
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("AsSalamu Alykom!");
                 });
             });
         }
